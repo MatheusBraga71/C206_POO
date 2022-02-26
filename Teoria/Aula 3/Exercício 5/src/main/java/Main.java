03c1ec163c730262271d82a46a2252ef5caca3fb
MatheusBraga71 <matheus_pivoto@gec.inatel.br>
2022-02-26 09:59:51 -0300

    Modificando o texto de saída.

--- Teoria/Aula 3/Exercício 5/src/main/java/Main.java
+++ Teoria/Aula 3/Exercício 5/src/main/java/Main.java
@@ -3,18 +3,22 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int numDigitado;
+        int numDigitado; //Variável para armazenar o número a ser comparado
 
-        Scanner entrada = new Scanner(System.in);
         Random rand = new Random();
-        int x = rand.nextInt(10) + 1;
+        int x = rand.nextInt(10) + 1; //Variável para armazenar o número aleatório
 
         System.out.println("Qual foi o número gerado?");
 
+        Scanner scan = new Scanner(System.in);
         do{
-            numDigitado = entrada.nextInt();
+            numDigitado = scan.nextInt();
+            if(numDigitado != x)
+                System.out.println("Tente novamente!");
 
         }while(x != numDigitado);
         System.out.println("Você Acertou!!");
+
+        scan.close();
     }
 }
