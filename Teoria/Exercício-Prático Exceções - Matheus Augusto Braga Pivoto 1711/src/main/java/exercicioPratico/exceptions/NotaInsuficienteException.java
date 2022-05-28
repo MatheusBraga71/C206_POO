3668bd14a107e21db2a9efffa67e750691d295ca
MatheusBraga71 <matheus_pivoto@gec.inatel.br>
2022-05-27 22:24:28 -0300

    falha

--- Teoria/Exercício-Prático Exceções - Matheus Augusto Braga Pivoto 1711/src/main/java/exercicioPratico/exceptions/NotaInsuficienteException.java
+++ Teoria/Exercício-Prático Exceções - Matheus Augusto Braga Pivoto 1711/src/main/java/exercicioPratico/exceptions/NotaInsuficienteException.java
@@ -1,6 +1,6 @@
 package exercicioPratico.exceptions;
 
-public class NotaInsuficienteException{
+public class NotaInsuficienteException {
 /*
     public NotaInsuficienteException(){
         System.out.println("Nota insuficiente para aprovar o projeto");
